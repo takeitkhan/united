b3e41763651ab47541d6eb2139952e32fb4b76a3
Shopnil <ahamadshopnil.dev@gmail.com>
2025-01-20 12:15:01 +0600

    add phone icon button for direct call feature

--- components/Contact.js
+++ components/Contact.js
@@ -7,18 +7,28 @@ import Link from "next/link";
 
 const Contact = () => {
   return (
-    <div className=" right-0 top-1/2 z-10 fixed">
+    <div className="right-0 top-1/2 z-10 fixed">
       <div className="flex flex-col gap-4">
+        {/* Email icon */}
         <Link
           href="/contact"
           className="bg-bgYellow p-2 rounded-md text-center flex items-center justify-center cursor-pointer"
         >
-          {" "}
           <MdEmail className="text-4xl" />
         </Link>
-        {/* <span className="bg-navBg p-2 block rounded-md text-white cursor-pointer">
-          <IoCallSharp className="text-4xl" />
-        </span> */}
+        {/* Phone icon with custom tooltip */}
+        <div className="relative group">
+          <a
+            href="tel:+8801988557711" // Bangladeshi phone number
+            className="bg-navBg p-2 block rounded-md text-white cursor-pointer"
+          >
+            <IoCallSharp className="text-4xl" />
+          </a>
+          {/* Tooltip positioned immediately to the left */}
+          <div className="absolute right-full top-1/2 transform -translate-y-1/2 mr-2 hidden group-hover:flex items-center justify-center bg-black text-white text-sm rounded-md py-1 px-2">
+            <span> 01988557711</span>
+          </div>
+        </div>
       </div>
     </div>
   );
