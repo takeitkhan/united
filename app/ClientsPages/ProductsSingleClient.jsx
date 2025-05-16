55802dde97194c6db39ec98dbf4c54784f2a9d0c
ahmadShopnil <ahmadshopnil75@gmail.com>
2025-05-16 11:16:34 +0600

    fix category sort problem, extra image not showing

--- app/ClientsPages/ProductsSingleClient.jsx
+++ app/ClientsPages/ProductsSingleClient.jsx
@@ -16,6 +16,7 @@ import GetAQuote from '@/components/GetAQuote'
 import { FaFileInvoice } from 'react-icons/fa6'
 import { MdDetails, MdPictureAsPdf } from 'react-icons/md'
 import ProductImage from '@/components/ProductImage'
+import { getMetaValueFromExtraFields } from '@/helpers/metaHelpers'
 
 function ProductSingleClient ({ slug }) {
   const [product, setProduct] = useState([]) // set product data
@@ -52,11 +53,13 @@ function ProductSingleClient ({ slug }) {
   if (loading) return <Loading />
   if (error) return <div>{error}</div>
 
-  const images =
-    Array.isArray(product.extra_fields[0]?.meta_value) &&
-    product.extra_fields[0]?.meta_value.length > 0
-      ? product.extra_fields[0].meta_value
-      : [product.featured_image] // If no extra fields, fallback to featured_image
+  // const images =
+  //   Array.isArray(product.extra_fields[0]?.meta_value) &&
+  //   product.extra_fields[0]?.meta_value.length > 0
+  //     ? product.extra_fields[0].meta_value
+  //     : [product.featured_image] // If no extra fields, fallback to featured_image
+ const images =getMetaValueFromExtraFields(product,"product_extra_images")
+    
 
   //const shortDesciption = stripHtmlTags();
 
@@ -89,7 +92,7 @@ function ProductSingleClient ({ slug }) {
             <div className='md:w-full'>
               <div className='md:flex md:justify-between gap-5'>
                 <div className='mx-auto p-5 border border-gray-200 w-full md:h-1/3'>
-                  <ProductImage product={product} />
+                  <ProductImage product={product} images={images} />
                 </div>
 
                 <div className='md:w-2/3'>
--- helpers/metaHelpers.js
+++ helpers/metaHelpers.js
@@ -19,3 +19,10 @@ export const getMediaLinkByMetaName = (jsonData, metaName) => {
 
   return null;
 };
+
+
+export const getMetaValueFromExtraFields = (jsonData, metaName) => {
+  // console.log("from metaFrom extra:",jsonData, metaName);
+  const foundItem = jsonData?.extra_fields?.find((item) => item.meta_name === metaName);
+  return foundItem ? foundItem.meta_value : null;
+};
